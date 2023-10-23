ac95d82635b993268eb7499c50e538ba16761982
willianosaida <willianosaida@hotmail.com>
2023-10-23 12:46:14 -0300

    TODO's solutions - Project finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "48984901765", modelo: "Nokia-3620", imei: "4825436236", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Maps");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "48984257650", modelo: "Iphone-XR-Plus", imei: "2854254243", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Waze");
